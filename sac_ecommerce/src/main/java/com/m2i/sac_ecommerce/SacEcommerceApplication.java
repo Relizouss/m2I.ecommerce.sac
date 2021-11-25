5546e2009dbe7fa4d863c1895d3152a0512b9cd2
Relizouss <51269460+Relizouss@users.noreply.github.com>
2021-11-25 20:42:40 +0100

    This project , is about E-commerce bags product. cette branche est crée dans la machine distante  M2I.

---
+++ sac_ecommerce/src/main/java/com/m2i/sac_ecommerce/SacEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.sac_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacEcommerceApplication.class, args);
+	}
+
+}
---
+++ sac_ecommerce/src/test/java/com/m2i/sac_ecommerce/SacEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.sac_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
